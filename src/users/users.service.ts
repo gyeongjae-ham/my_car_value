4dc783094a3a87b7077dc33eb304f07958c88e55
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-03 17:13:30 +0900

    Added why repository use create and save method.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,7 +8,7 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
-    const user = this.repo.create({ email, password });
+    const user = this.repo.create({ email, password }); // createUserDto의 instance가 생성되고 create 된 다음 save해야 데이터 베이스에 저장 됨
 
     return this.repo.save(user);
   }
