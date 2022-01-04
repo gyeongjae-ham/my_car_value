f492c110af94231f9d4fe713bc38af5ae47451b5
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-04 16:37:39 +0900

    Modified signup logic.

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -27,8 +27,10 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex');
 
     // Create a new user and save it
+    const user = await this.userService.create(email, result);
 
     // return the user
+    return user;
   }
 
   signin() {}
