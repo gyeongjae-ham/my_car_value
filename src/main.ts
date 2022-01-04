788a7d7e553c68ff6d2b55b2eb90212c4c035ada
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-04 17:53:37 +0900

    Added signout with cookie-session.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdfasdf'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // request로 들어오는 정보가 유효한 정보인지 필터링 해줌
