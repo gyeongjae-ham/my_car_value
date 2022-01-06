7f0eb23dd284e43b761535d111997adaccb0b3b7
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-06 16:32:53 +0900

    Added fakeUsersService instance.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   // create a fake copy of users service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
+    // fake userService instance를 만들기 위해서 typescript의 도움을 받기 위해 타입을
+    // UsersService로 지정해 주는데, authService에 해당하는 userService만 구현하기 위해서 Partial 부분을 작성해서 부분 작성이라는 것을 표시해준다
+    // create method는 원래 User 객체를 반환해야 하는데 Dto에 보면 로그를 남기는 부분이 있어서 에러가 발생한다 테스트 할 때 로그까지 구현하고 싶지 않기 때문에
+    // 객체를 as User로 받아서 객체를 반환하는 것처럼 속여서 작성한다
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
