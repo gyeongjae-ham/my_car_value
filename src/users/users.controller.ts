37d00262a08ef5787aa6ef14022eb81306048619
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-07 11:05:44 +0900

    Added users.controller test.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,9 @@ import {
   Post,
   Get,
   Patch,
-  Query,
-  Param,
   Delete,
+  Param,
+  Query,
   NotFoundException,
   Session,
   UseGuards,
@@ -14,18 +14,18 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorator/current-user.decorator';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
   constructor(
-    private userService: UsersService,
+    private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
@@ -56,26 +56,25 @@ export class UsersController {
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    const user = await this.userService.findOne(parseInt(id));
-
+    const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('user not found');
     }
     return user;
   }
 
   @Get()
   findAllUsers(@Query('email') email: string) {
-    return this.userService.find(email);
+    return this.usersService.find(email);
   }
 
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
-    return this.userService.remove(parseInt(id));
+    return this.usersService.remove(parseInt(id));
   }
 
   @Patch('/:id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.userService.update(parseInt(id), body);
+    return this.usersService.update(parseInt(id), body);
   }
 }
