84158bb377714e0a71e7abff387417ca26a38ce5
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-07 16:39:39 +0900

    To enable repetitive tests.

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,18 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('handles a signup request', () => {
+    const email = 'test1@test.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '12341234' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
 });
